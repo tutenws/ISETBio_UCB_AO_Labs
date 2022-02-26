29f9a5a23d48b0cb6d5ca96d91ae28204b800a38
David Brainard <brainard@psych.upenn.edu>
2022-02-26 12:35:09 -0500

    Add ToolboxToolbox configuration
    As the short comment says.

---
+++ configuration/ISETBio_UCB_AO_LabsLocalHookTemplate.m
@@ -0,0 +1,53 @@
+function ISETBio_UCB_AO_LabsLocalHook
+% ISETBio_UCB_AO_LabsLocalHook
+%
+% Configure things for working on the ISETBio_UCB_AO_Labs project.
+%
+% For use with the ToolboxToolbox.  If you copy this into your
+% ToolboxToolbox localToolboxHooks directory (by defalut,
+% ~/localToolboxHooks) and delete "LocalHooksTemplate" from the filename,
+% this will get run when you execute tbUseProject('ISETBio_UCB_AO_Labs') to set up for
+% this project.  You then edit your local copy to match your configuration.
+%
+% You will need to edit the project location and i/o directory locations
+% to match what is true on your computer.
+
+%% Say hello
+theProject = 'ISETBio_UCB_AO_Labs';
+fprintf('Running %s local hook\n',theProject);
+
+%% Remove old preferences
+if (ispref(theProject))
+    rmpref(theProject);
+end
+
+%% Put project toolbox onto path
+%
+% Specify project name and location
+projectName = theProject;
+projectBaseDir = tbLocateProject(theProject);
+
+%% Figure out where baseDir for other kinds of data files is.
+sysInfo = GetComputerInfo();
+switch (sysInfo.localHostName)
+    case 'eagleray'
+        % DHB's old desktop
+        baseDir = fullfile(filesep,'Volumes','Users1','Dropbox (Aguirre-Brainard Lab)');
+ 
+    otherwise
+        % Some unspecified machine, try user specific customization
+        switch(sysInfo.userShortName)
+            % Could put user specific things in, but at the moment generic
+            % is good enough.
+            otherwise
+                baseDir = fullfile('/Users',sysInfo.userShortName,'Dropbox (Aguirre-Brainard Lab)');
+        end
+end
+
+%% Set preferences for project i/o
+
+% This is where the output goes
+analysisDir = fullfile(baseDir,'ISETBio_UCB_AO_Labs',theProject);
+
+% Set the preferences
+setpref(theProject,'analysisDir',analysisDir);
